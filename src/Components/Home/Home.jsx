035fbe715bc99d7044f05ebc705ebffbb1475e3d
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-04-02 23:53:34 +0530

    Resume update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -83,7 +83,7 @@ const Home = ({ scrollRef }) => {
 					<h1>{intro}</h1>
 					<div className={styles.btn}>
 						<a
-							href="/ShaikShakeer_Resume.pdf"
+							href="/Shaik Shakeer_Resume.pdf"
 							target='_blank'
 							rel='noreferrer'
 						>
